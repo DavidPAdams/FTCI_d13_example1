ff0bc0223ec1a07c0225f1cd5b400b8389f310c1
David Adams <david.riding.rails@gmail.com>
2020-03-23 06:48:10 -0700

    day 13 example project

---
+++ src/main/java/com/tts/d13_myAwesomeProject/D13MyAwesomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tts.d13_myAwesomeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D13MyAwesomeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D13MyAwesomeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/d13_myAwesomeProject/D13MyAwesomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.d13_myAwesomeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D13MyAwesomeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
